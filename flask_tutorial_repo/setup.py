d8f15e4ac66bc6f1ab4777fcf348ec0e053da385
nour <nourmohamedelhwary@gmail.com>
2023-02-01 07:54:04 +0200

    no message

---
+++ flask_tutorial_repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='build rest api with flask, connextion and SQLAlchemy ',
+    author='Nour',
+    license='MIT',
+)
